18a7627e67024c1169f0381b969f592fcfa324ac
grossiro-code <126162667+grossiro-code@users.noreply.github.com>
2023-03-26 18:22:30 +0200

    Update mychat.py

--- mychat.py
+++ mychat.py
@@ -10,17 +10,17 @@ import streamlit as st
 
 openai.api_key = st.secrets["pass"]
 # Create Text Area Widget to enable user to enter texts
-article_text = st.text_area(“Enter your scientific texts to summarize”)
+article_text = st.text_area("Enter your scientific texts to summarize")
 # Create Radio Buttons
-output_size = st.radio( label = “What kind of output do you want?”, 
-                        options= [“To-The-Point”, “Concise”, “Detailed”]
+output_size = st.radio( label = "What kind of output do you want?", 
+                        options= ["To-The-Point", "Concise", "Detailed"]
                      )
 # First, we'll use an if statement to determine the desired output size 
 # and set the out_token variable accordingly:
 
-if output_size == “To-The-Point”:
+if output_size == "To-The-Point":
  out_token = 50
-elif output_size == “Concise”:
+elif output_size == "Concise":
  out_token = 128
 else:
  out_token = 516
